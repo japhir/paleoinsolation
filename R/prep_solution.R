c9f336a4bc9e0ceef14b08f1b5854c0c3c8566fe
Ilja J. Kocken <ikocken@hawaii.edu>
2024-12-05 13:23:08 -1000

    update prep_solution to use snvec to calculate omegabar

--- R/prep_solution.R
+++ R/prep_solution.R
@@ -1,21 +1,26 @@
-prep_solution <- function(astronomical_solution = "PT-ZB18a(1,1)") {
-  dat <- get_solution(astronomical_solution)
-  sol <- stringr::str_extract(astronomical_solution, "^PT-(ZB\\d{2}[a-z])\\(", group = 1)
-  if (sol == "ZB18a") {
-    sol <- "ZB18a-100"
-  }
-  ecc <- get_solution(sol)
-
-  # TODO: instead just apply snvec with output = "all"?
-  dat <- dplyr::mutate(dat,
-                       # interpolate ecc to this time scale
-                       ecc = stats::approx(ecc$time, ecc$ecc, xout = .data$time)$y,
-                       # inversely calculate varpi
-                       # cp = e * sin(omegabar)
-                       # cp / e = sin(omegabar)
-                       # asin(cp / e) = omegabar
-                       # NOTE: this does result in NaN sometimes!!
-                       omegabar = asin(cp / ecc))
-
-  select(dat, all_of(c("time", "ecc", "epl", "phi", "omegabar", "cp")))
+prep_solution <- function(orbital_solution = "full-ZB18a", tend = -10e3, ed = 1, td = 1) {
+  R2D <- 180.0 / pi
+  OMT <- 75.5940
+  refsln <- snvec(tend, ed, td, orbital_solution, output = "all") |>
+    mutate(time = time * 1e3) |> # time in years so it's compatible with older stuff
+    # is it omegabar or varpi* or varpi???
+    # ber78 says 'varpi' true solar longitude of perihelion
+    # so should just be lph ?
+    # omegabar = varpi + | phi |
+    # so varpi = lphui + OMT => rewrapped
+    mutate(varpi = (lphi + OMT) / R2D) |>
+    mutate(omegabar = varpi - phi) |> # what snvec does
+    ## mutate(omegabar = varpi + abs(phi)) |> # what it says on the tin
+    # crucifix calls omegabar or pibar varpi
+    # maybe we need this line from https://github.com/mcrucifix/palinsol/blob/master/data-raw/LA04.R
+    ## la04past['varpi'] <- (la04past['varpi'] - pi ) %% (2*pi)
+    mutate(omegabar = (omegabar - pi) %% (2 * pi)) |> # what Crucifix does in his prep
+    rename(ecc = eei)
+  # calculate 65°N summer solstice insolation
+  refsln <- refsln |> mutate(insol = insolation(refsln$ecc, refsln$epl, refsln$omegabar,
+                                                longitude = pi / 2, latitude = 65 * pi / 180, S0 = 1361))
+  refsln |> select(all_of(c("time",
+                            "sx", "sy", "sz",
+                            "nnx", "nny", "nnz",
+                            "ecc", "epl", "phi", "cp", "varpi", "omegabar", "insol")))
 }
